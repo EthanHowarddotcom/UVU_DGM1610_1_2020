73bb3d5a50b4e5b5e1d5751424dd82f678050e11
Ethan Howard <ethan.h134@stu.nebo.edu>
2020-09-23 12:25:59 -0600

    Submitting Driving sim game

---
+++ Driving_Sim/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //access modifier type name operator value
+    private int speed = 75;
+    float turnSpeed = 50;
+    
+    float horizontalInput;
+    float forwardInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+        //Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Rotates vehicle left and right
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+    }
+}

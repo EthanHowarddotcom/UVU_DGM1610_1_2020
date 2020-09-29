e889e7981044c5a31a7aea1f1014d2fdf08ce507
Ethan Howard <ethan.h134@stu.nebo.edu>
2020-09-29 12:03:15 -0600

    Submitting Flying Sim

---
+++ flying_sim/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    public float xAngle, yAngle, zAngle;
+    public GameObject Cam;
+    private Vector3 offset = new Vector3(-100, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cam.transform.Rotate(0.0f, 90.0f, 0.0f, Space.Self);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ flying_sim/Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float xAngle, yAngle, zAngle;
+   public GameObject propeller; 
+    void Start()
+    {
+        
+        
+        propeller.transform.Rotate(90.0f, 0.0f, 0.0f, Space.Self);
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        propeller.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+    }
+}

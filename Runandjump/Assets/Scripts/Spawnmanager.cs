e87146ff86855f070c2cf23718a4b2e32584bc29
Ethan Howard <ethan.h134@stu.nebo.edu>
2020-12-02 12:02:39 -0700

    i forgot what this one is

---
+++ Runandjump/Assets/Scripts/Spawnmanager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnmanager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(30, 0, 0);
+    private float startDelay = 2;
+    private float restartDelay = 2;
+
+    private PlayerController playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawnobstacal", startDelay, restartDelay);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    void Spawnobstacal()
+    {
+        if(playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+    }
+}

cdcba27b3e6ea233da3786e488abee47f702f29b
Ethan Howard <ethan.h134@stu.nebo.edu>
2020-12-16 12:09:14 -0700

    Sumo game

---
+++ Sumo/Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+    private GameObject FocalPoint;
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        FocalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(FocalPoint.transform.forward * speed * forwardInput);
+    }
+}

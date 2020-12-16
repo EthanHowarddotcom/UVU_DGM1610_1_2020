e88b19bba1d210dd5407e5646e9163b5be1af363
Ethan Howard <ethan.h134@stu.nebo.edu>
2020-12-16 12:36:36 -0700

    Sumo update

--- Sumo/Assets/Course Library/Scripts/PlayerController.cs
+++ Sumo/Assets/Course Library/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float speed = 5.0f;
     private GameObject FocalPoint;
+    public bool hasPowerUp
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -19,5 +20,24 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(FocalPoint.transform.forward * speed * forwardInput);
+    }
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.CompareTag("Powerup")){
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+        }
+    }
+    private float powerupStrength = 15.0f
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("enemy") && hasPowerUp) 
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>(); 
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Player collided with " + collision.gameObject + "with powerup set to " + hasPowerUp)
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse); 
+        }
     }
 }

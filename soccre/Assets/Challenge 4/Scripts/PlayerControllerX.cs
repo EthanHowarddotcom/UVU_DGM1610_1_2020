66e87281a2dc0ddd62c4c608670ce0c66a023d47
Ethan Howard <ethan.h134@stu.nebo.edu>
2021-01-13 11:53:59 -0700

    finishedsoccre

--- soccre/Assets/Challenge 4/Scripts/PlayerControllerX.cs
+++ soccre/Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -29,6 +29,7 @@ public class PlayerControllerX : MonoBehaviour
 
         // Set powerup indicator position to beneath player
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+        
 
     }
 
@@ -40,6 +41,7 @@ public class PlayerControllerX : MonoBehaviour
             Destroy(other.gameObject);
             hasPowerup = true;
             powerupIndicator.SetActive(true);
+            StartCoroutine("PowerupCooldown"); 
         }
     }
 
@@ -57,7 +59,7 @@ public class PlayerControllerX : MonoBehaviour
         if (other.gameObject.CompareTag("Enemy"))
         {
             Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
-            Vector3 awayFromPlayer =  transform.position + other.gameObject.transform.position; 
+            Vector3 awayFromPlayer =  other.gameObject.transform.position - transform.position; 
            
             if (hasPowerup) // if have powerup hit enemy with powerup force
             {
